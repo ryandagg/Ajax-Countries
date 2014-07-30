955a1a59d7dee9ae362dd8d8045517c1f9165606
ryandagg <ryan.dagg@gmail.com>
2014-07-30 16:40:58 -0600

    Step 4

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
+var mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost/countries');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', indexController.index);
 
 var server = app.listen(7004, function() {
---
+++ public/scripts/main.js
@@ -0,0 +1,6 @@
+// console.log("Test!");
+// console.log("(typeof $().emulateTransitionEnd == 'function'):", (typeof $().emulateTransitionEnd == 'function'))
+// 
+$(function(){
+
+})
